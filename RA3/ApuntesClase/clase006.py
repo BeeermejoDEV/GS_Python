58c5829b729dd1a1d9e572c00843e4b331713d82
David <beeermejoDev@gmail.com>
2025-10-27 22:12:48 +0100

    multiples parametros

---
+++ RA3/ApuntesClase/clase006.py
@@ -0,0 +1,17 @@
+"""
+Multiples parámetros
+"""
+
+
+def multiplicar3Numeros(a,b,c):
+    return a*b*c
+
+a= int(input("Introduce un numero a \n"))
+b= int (input("Introduce un numero b \n"))
+c= int (input("Introduce un numero c \n"))
+
+resultado= multiplicar3Numeros(a,b,c)
+
+
+
+print (resultado)
