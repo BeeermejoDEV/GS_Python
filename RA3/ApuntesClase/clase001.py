196a212b54dc79296a975fed066a3781148c3f37
David <beeermejoDev@gmail.com>
2025-10-27 21:25:48 +0100

    ej1 practicado en casa

--- RA3/ApuntesClase/clase001.py
+++ RA3/ApuntesClase/clase001.py
@@ -1,20 +1,5 @@
-"""Si variable, devuelve algo sin pedir nada"""
+def saludar():
+    print("Hola!")
+    print("Bienvenido al curso de Python.")
 
-def cosa():
-    print ("hola")
-    print ("cosa")
-
-cosa()
-print("________")
-cosa()
-
-
-"""Dandole un valor nosotros"""
-def mayorMenor(edad):
-    if edad >=18:
-        print ("mayor de edad")
-    else:
-        print ("menor de dad")
-
-
-mayorMenor(20)
+saludar()
