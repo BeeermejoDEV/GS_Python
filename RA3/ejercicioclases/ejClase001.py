f82414f3e36079c0f172f2595251bd345b4c7844
David <beeermejoDev@gmail.com>
2025-10-27 23:36:07 +0100

    deberes

---
+++ RA3/ejercicioclases/ejClase001.py
@@ -0,0 +1,5 @@
+"""
+Ejercicio 6:
+Reescribe tu programa de cálculo de pago con el tiempo extra (hora y media) 
+y crea una función llamada computepay, la cual reciba dos parámetros (horas y tarifa).
+"""
---
+++ RA3/ejercicioclases/ejClase002.py
@@ -0,0 +1,6 @@
+"""
+Ejercicio 7:
+Reescribe el programa de calificaciones del capítulo anterior utilizando una función 
+llamada computegrade, que reciba una puntuación (score) como parámetro y devuelva la 
+nota (grade) como cadena de texto.
+"""
