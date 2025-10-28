76565e426c620c0075dc2f1de12b02b11efc32a8
David <beeermejoDev@gmail.com>
2025-10-28 09:22:11 +0100

    ejercicios de clase resueltos

--- RA3/ejercicioclases/ejClase001.py
+++ RA3/ejercicioclases/ejClase001.py
@@ -3,3 +3,17 @@ Ejercicio 6:
 Reescribe tu programa de cálculo de pago con el tiempo extra (hora y media) 
 y crea una función llamada computepay, la cual reciba dos parámetros (horas y tarifa).
 """
+
+def computePay(horas,tarifa):
+    if (horas>40):
+        horasExtras= horas-40
+        salario = (horasExtras*(tarifa*1.5))+((horas-horasExtras)*tarifa)
+        print (f"Tu salario es de: {salario}")
+    else:
+        salario = horas*tarifa
+        print (f"tu salario es de: {salario}")
+
+horasUsu= int (input("Escriba cuantas horas trabaja por favor: \n"))
+tarifaUsu= int (input("Escribe a cuanto te pagan la hora porfavor: \n"))
+
+computePay(horasUsu, tarifaUsu)
--- RA3/ejercicioclases/ejClase002.py
+++ RA3/ejercicioclases/ejClase002.py
@@ -4,3 +4,26 @@ Reescribe el programa de calificaciones del capítulo anterior utilizando una fu
 llamada computegrade, que reciba una puntuación (score) como parámetro y devuelva la 
 nota (grade) como cadena de texto.
 """
+
+
+def computeGrade(score):
+    if (score<10 and score>0):#Aqui puse or en vez de and. mayor que 0 ,ó menor que 10 siempre son TRUE
+        if (score<5):
+            print ("Has sacado un insuficiente: ")
+            print ((f"Su nota a sido un {score} sobre 10"))
+        elif (score>=5 and score<7):
+            print ("Has sacado un suficiente: ")
+            print ((f"Su nota a sido un {score} sobre 10"))
+        elif (score>=7 and score<9):
+            print ("Has sacado un notable: ")
+            print ((f"Su nota a sido un {score} sobre 10"))
+        else:
+            print ("Has sacado un sobresaliente: ")
+            print ((f"Su nota a sido un {score} sobre 10"))
+
+    else:
+        print(f"error al introducir la nota, no puede ser menor de 0 o mayor que 10")
+
+
+notaUsu=float(input ("Escriba su nota: "))
+computeGrade(notaUsu)
